4fa9be1e7ad6bbd0ee48acee8e9eff8f1e45aa79
Elliot Brock <beta.redex@gmail.com>
2018-01-02 13:43:12 -0500

    comments, switch from lua to js on the client-side

--- event.go
+++ event.go
@@ -4,6 +4,8 @@ type Event struct {
   timestamp uint // when the event was created
   schedule uint // when the event is scheduled to resolve
   method string // method of the event (e.g. pause)
+  owner uint // id of owner client
+  vidTime uint // what time the video was at when the event happened
 }
 
 /*
